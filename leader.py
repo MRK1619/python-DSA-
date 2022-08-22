15054e27f176248b989571423161ba1dabb6e8ac
Partha Sarathi Hazra <sarathiparthahazra9609@gmail.com>
2022-08-22 12:00:55 +0530

    first commit

---
+++ leader.py
@@ -0,0 +1,15 @@
+def leader(arr, size):
+    max_from_left = arr[size-1]
+    print(max_from_left, end=' ')
+    for i in range(size-2, -1, -1):
+     if max_from_left < arr[i]:
+        print(arr[i], end=' ')
+        max_from_left = arr[i]
+arr=list(map(int, input().split()))
+size=len(arr)
+leader(arr,size)
+    
+    
+
+
+
