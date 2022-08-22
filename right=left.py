c2d9bc4cae4d105cef9e1ce56635f655dc74b41a
Partha Sarathi Hazra <sarathiparthahazra9609@gmail.com>
2022-08-22 12:15:52 +0530

    Equilibram

---
+++ right=left.py
@@ -0,0 +1,21 @@
+def equilibram(arr,size):
+    i,start,end,left_sum,right_sum=0,0,size-1,0,0
+    for i in range(size):
+        if(start== end and right_sum==left_sum):
+            return arr[start]
+        if(start==end):
+            return -1
+        if(left_sum>right_sum):
+            right_sum +=arr[end]
+            end -= 1
+        elif(left_sum<right_sum):
+            left_sum +=arr[start]
+            start += 1
+        else:
+            right_sum +=arr[end]
+            end -= 1
+    if (not i):
+        return arr[0]
+arr=list(map(int,input().split()))
+size=len(arr)
+print(equilibram(arr,size))
